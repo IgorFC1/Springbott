c1f247acc83704a019735b663f47e95a03db4a51
IgorFC1 <igorfcerqueira08@gmail.com>
2020-06-09 12:52:02 -0300

    SpringBot

---
+++ springboot/src/test/java/igor/trabalho/springboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.trabalho.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
